5bd115eda8e10ffcfbdaae8b2ebd13ea7ea05441
Ilya <ilya.w.a.j@gmail.com>
2023-01-08 01:34:15 +0400

    signup signin forms

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import queryString from 'query-string'
-
+ 
 const baseURL = "http://localhost:5000/api/v1/"
 
 const privateClient = axios.create({
--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom"
 import GlobalLoading from "../common/GlobalLoading";
 import Footer from "../common/Footer"
 import Topbar from "../common/Topbar"
+import AuthModal from "../common/AuthModal";
 
 
 const MainLayout = () => {
@@ -13,6 +14,7 @@ const MainLayout = () => {
         <GlobalLoading />
 
         {/* login modal */}
+        <AuthModal />
         {/* login modal */}
         <Box display="flex" minHeight="100vh">
           {/* header */}
